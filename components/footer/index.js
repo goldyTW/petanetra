29b7e9031319ae9ca59816301343bddb9e32fdb4
Goldy Tanjung Wijaya <goldywijaya@gmail.com>
2023-01-12 11:07:27 +0700

    fix bug noreferrer

--- components/footer/index.js
+++ components/footer/index.js
@@ -73,7 +73,7 @@ function Footer() {
                 </div>
                 <div className='row justify-content-center'>
                     <div className="col-12 text-center py-4">
-                        <span className="copyright">© Copyright {year} by PetaNetra Team | Made by <a href='https://websitesimple.id/' target="_blank" style={{textDecoration:'none', fontWeight:'bold', color:'white'}}>WebsiteSimple.id</a></span>
+                        <span className="copyright">© Copyright {year} by PetaNetra Team | Made by <a href='https://websitesimple.id/' target="_blank" rel="noreferrer" style={{textDecoration:'none', fontWeight:'bold', color:'white'}}>WebsiteSimple.id</a></span>
                     </div>
                 </div>
                 </div>
